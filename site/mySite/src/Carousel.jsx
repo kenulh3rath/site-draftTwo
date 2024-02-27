5c0f97481ac2448d74b1bb00b8fccc9bf856d00a
Kenul Herath <kenulnavindu@gmail.com>
2024-02-27 22:25:30 +0000

    CertsAdd

--- site/mySite/src/Carousel.jsx
+++ site/mySite/src/Carousel.jsx
@@ -6,12 +6,9 @@ import { Carousel } from 'flowbite-react';
 export default function Certs() {
     return (
         <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-            <Carousel slideInterval={5000}>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+            <Carousel slideInterval={3000}>
+                <img src="/Cert_W3schools_CSharp.jpg" alt="C# Certification" className="w-3/4 h-auto" />
+                <img src="/Cert_Arduino.jpg" alt="Arduino Certification" className="w-3/4 h-auto" />
             </Carousel>
         </div>
     );
--- site/mySite/src/Footer.jsx
+++ site/mySite/src/Footer.jsx
@@ -9,7 +9,7 @@ export default function SiteFooter() {
                     <Footer.Copyright href="#" by="KenzTheDev™" year={2024} />
                     <Footer.LinkGroup>
                         {/*CV*/}
-                        <Footer.Link href="#">
+                        <Footer.Link href="/CV.pdf" download="Kenul-Herath-CV-2024">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                  className="bi bi-file-earmark-person" viewBox="0 0 16 16">
                                 <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
